bfbbfe2d19ee3a5eeb146bd355e5f47fee4c1a64
SohailKhan5281 <SohailKhan5281@gmail.com>
2024-03-05 11:44:53 +0530

    first project commit

---
+++ PenDroid/modules/banner.py
@@ -0,0 +1,37 @@
+from modules import color
+
+# version = "v1.61"
+
+menu1 = f"""
+   
+    {color.WHITE}1. {color.YELLOW}Tester Name                                           {color.WHITE}11. {color.YELLOW}Open a Link on Device                            {color.WHITE}21. {color.YELLOW}Access Device Shell                                            
+    {color.WHITE}2. {color.YELLOW}List Connected Devices                                {color.WHITE}12. {color.YELLOW}Display a Photo on Device                        {color.WHITE}22. {color.YELLOW}List All Folders/Files                    
+    {color.WHITE}3. {color.YELLOW}Disconnect All Devices                                {color.WHITE}13. {color.YELLOW}Play a Video on Device                           {color.WHITE}23. {color.YELLOW}Copy WhatsApp Data                    
+    {color.WHITE}4. {color.YELLOW}Mirror & Control Device                               {color.WHITE}14. {color.YELLOW}Get Device Information                           {color.WHITE}24. {color.YELLOW}Copy All Screenshots                     
+    {color.WHITE}5. {color.YELLOW}Get Screenshot                                        {color.WHITE}15. {color.YELLOW}Get Battery Information                          {color.WHITE}25. {color.YELLOW}Copy All Camera Photos                       
+    {color.WHITE}6. {color.YELLOW}Screen Record                                         {color.WHITE}16. {color.YELLOW}Restart Device                                   {color.WHITE}26. {color.YELLOW}Anonymous Screenshot            
+    {color.WHITE}7. {color.YELLOW}Download File/Folder from Device                      {color.WHITE}17. {color.YELLOW}Advanced Reboot Options                          {color.WHITE}27. {color.YELLOW}Extract APK from Installed App                  
+    {color.WHITE}8. {color.YELLOW}Send File/Folder to Device                            {color.WHITE}18. {color.YELLOW}Lock Device                                      {color.WHITE}28. {color.YELLOW}Stop ADB Server                             
+    {color.WHITE}9. {color.YELLOW}Run an App                                            {color.WHITE}19. {color.YELLOW}Uninstall an App                                 {color.WHITE}29. {color.YELLOW}Power Off Device   
+    {color.WHITE}10. {color.YELLOW}Anonymous Screen Record                              {color.WHITE}20. {color.YELLOW}List Installed Apps                              {color.WHITE}30. {color.YELLOW}Create Final Report"""
+
+
+ 
+
+menu = [menu1]
+
+banner2 ="" 
+
+banner_list = [banner2,]
+
+keycode_menu = f"""
+    {color.WHITE}1. {color.YELLOW}Keyboard Text Input                {color.WHITE}11. {color.YELLOW}Enter
+    {color.WHITE}2. {color.YELLOW}Home                               {color.WHITE}12. {color.YELLOW}Volume Up
+    {color.WHITE}3. {color.YELLOW}Back                               {color.WHITE}13. {color.YELLOW}Volume Down          
+    {color.WHITE}4. {color.YELLOW}Recent Apps                        {color.WHITE}14. {color.YELLOW}Media Play           
+    {color.WHITE}5. {color.YELLOW}Power Button                       {color.WHITE}15. {color.YELLOW}Media Pause
+    {color.WHITE}6. {color.YELLOW}DPAD Up                            {color.WHITE}16. {color.YELLOW}Tab 
+    {color.WHITE}7. {color.YELLOW}DPAD Down                          {color.WHITE}17. {color.YELLOW}Esc
+    {color.WHITE}8. {color.YELLOW}DPAD Left           
+    {color.WHITE}9. {color.YELLOW}DPAD Right
+    {color.WHITE}10. {color.YELLOW}Delete/Backspace"""
